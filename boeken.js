61f31fe22ca0b854ea36a4b4d5406bac945623cb
Pepijn-de-koning <54800145+Pepijn-de-koning@users.noreply.github.com>
2020-10-27 14:59:58 +0100

    Video 3 Done

--- boeken.js
+++ boeken.js
@@ -1,12 +1,24 @@
 const uitvoer = document.getElementById('boeken');
-
 const xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = () => {
   if(xhr.readyState == 4 && xhr.status ==200) {
     let resultaat = JSON.parse(xhr.responseText);
-    console.log(resultaat);
+    boeken.data = resultaat
+    boeken.uitvoeren();
   }
 }
 xhr.open('GET', 'boeken.json', true);
 xhr.send();
+
+const boeken = {
+
+  uitvoeren() {
+    let html = "";
+    this.data.forEach(boek => {
+      html += `<h3>${boek.titel}</h3>`
+    });
+    uitvoer.innerHTML = html
+  }
+
+}
