b7093ab14eb90f6406e73fa9e8e68c20986f7dd8
Pepijn-de-koning <54800145+Pepijn-de-koning@users.noreply.github.com>
2020-10-27 15:29:34 +0100

    Video 6 Done

--- boeken.js
+++ boeken.js
@@ -26,6 +26,10 @@ const boeken = {
       html += `<section class="boek">`;
       html += `<img class="boek__cover" src="${boek.cover}">`;
       html += `<h3>${titel}</h3>`;
+      html += `<span class="boek__uitgave"> - uitgave: ${boek.uitgave}<br></span>`
+      html += `<span class="boek__ean"> - ean: ${boek.ean}<br></span>`
+      html += `<span class="boek__taal"> - Taal: ${boek.taal}<br></span>`
+      html += `<span class="boek__paginas"> - Paginas: ${boek.paginas} blz.<br></span>`
       html += `</section>`;
     });
     uitvoer.innerHTML = html
