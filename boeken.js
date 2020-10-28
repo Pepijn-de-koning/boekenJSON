2787d3a3379120276cbab0cb9876c68871c606ea
Pepijn-de-koning <54800145+Pepijn-de-koning@users.noreply.github.com>
2020-10-28 15:58:01 +0100

    Video 11 Done

--- boeken.js
+++ boeken.js
@@ -37,14 +37,38 @@ const boeken = {
       html += `<img  class="boek__cover" src="${boek.cover}">`;
       html += `<h3   class="boek__kop">${titel}</h3>`;
       html += `<p    class="boek__auteurs">${auteurs}</p>`;
-      html += `<span class="boek__uitgave">${boek.uitgave} | </span>`
-      html += `<span class="boek__ean">${boek.ean} | </span>`
-      html += `<span class="boek__taal">${boek.taal} | </span>`
-      html += `<span class="boek__paginas">${boek.paginas} paginas<br></span>`
-      html += `<span class="boek__prijs">${boek.prijs.toLocaleString('nl-NL', {currency: 'EUR', style: 'currency'})}</span>`
+      html += `<span class="boek__uitgave">${this.datumOmzetten(boek.uitgave)}</span>`;
+      html += `<span class="boek__ean">${boek.ean}</span>`;
+      html += `<span class="boek__taal">${boek.taal}</span>`;
+      html += `<span class="boek__paginas">${boek.paginas} paginas<br></span>`;
+      html += `<span class="boek__prijs">${boek.prijs.toLocaleString('nl-NL', {currency: 'EUR', style: 'currency'})}</span>`;
       html += `</section>`;
     });
     uitvoer.innerHTML = html
+  },
+  datumOmzetten(datumString) {
+    let datum = new Date(datumString);
+    let jaar = datum.getFullYear();
+    let maand = this.geefMaandnaam(datum.getMonth());
+    return `${maand} ${jaar}`;
+  },
+  geefMaandnaam(m) {
+    let maand = "";
+    switch (m) {
+      case 0 : maand = 'januari'; break;
+      case 1 : maand = 'februari'; break;
+      case 2 : maand = 'maart'; break;
+      case 3 : maand = 'april'; break;
+      case 4 : maand = 'mei'; break;
+      case 5 : maand = 'juni'; break;
+      case 6 : maand = 'juli'; break;
+      case 7 : maand = 'augustus'; break;
+      case 8 : maand = 'september'; break;
+      case 9 : maand = 'oktober'; break;
+      case 10 : maand = 'november'; break;
+      case 11 : maand = 'december'; break;
+      default : maand = m;
+    }
+    return maand;
   }
-
 }
